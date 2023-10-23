149f8a7118bfec0d2fccffc26da4072f8993bed3
totlevi <tothlevente4321@gmail.com>
2023-10-23 13:28:13 +0200

    😡
    😡😡😡

--- Application/Domain/DTOs/TransferRequestDto.cs
+++ Application/Domain/DTOs/TransferRequestDto.cs
@@ -3,8 +3,8 @@ namespace Domain.DTOs;
 public class TransferRequestDTO
 {
     public decimal Amount { get; set; }
-    public int RecipientName { get; set; }
+    public string RecipientName { get; set; }
     public int RecipientAccountNumber { get; set; }
     public string Message { get; set; }
-    public string SenderAccountNumber { get; set; }
+    public int SenderAccountNumber { get; set; }
 }
