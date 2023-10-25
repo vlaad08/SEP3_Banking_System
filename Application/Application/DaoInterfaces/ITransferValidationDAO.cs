de82408afe44de260486446cfa2c27ec5b776528
totlevi <tothlevente4321@gmail.com>
2023-10-25 12:49:54 +0200

    Data base acces setup

--- Application/Application/DaoInterfaces/ITransferValidationDAO.cs
+++ Application/Application/DaoInterfaces/ITransferValidationDAO.cs
@@ -4,6 +4,6 @@ namespace Application.DaoInterfaces;
 
 public interface ITransferValidationDAO
 {
-    public Task<TransferInfo> CreateAsync(TransferInfo transferInfo);
-    public Task<IEnumerable<TransferInfo>> GetAsync();//?? do i need to get this fucker back?
+    //public Task<TransferInfo> CreateAsync(TransferInfo transferInfo);
+    //public Task<IEnumerable<TransferInfo>> GetAsync();//?? do i need to get this fucker back?
 }   
---
+++ Application/DataAccess/DBComm/DataBaseAcces.cs
@@ -0,0 +1,8 @@
+using Application.DaoInterfaces;
+
+namespace DataAccess.DBComm;
+
+public class DataBaseAcces : IDataBaseAccess
+{
+    
+}
