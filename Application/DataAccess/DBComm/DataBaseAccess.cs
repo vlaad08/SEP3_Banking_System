b9119b1dd12e1a5173c23cfb3bba1fc44d1f0957
totlevi <tothlevente4321@gmail.com>
2023-11-01 14:36:40 +0100

    started doing the client for grpc

--- Application/DataAccess/DBComm/DataBaseAccess.cs
+++ Application/DataAccess/DBComm/DataBaseAccess.cs
@@ -3,23 +3,31 @@ using System.Text;
 using System.Text.Json;
 using Application.DaoInterfaces;
 using Domain.DTOs;
+using Grpc.Core;
+using Grpc.Net.Client
+using Grpc.Net.ClientFactory;
 
 namespace DataAccess.DBComm;
 
 public class DataBaseAccess : IDataBaseAccess
 {
-    private HttpClient rest=new HttpClient();
+    //private HttpClient rest=new HttpClient();
+    
+    private readonly GrpcChannel channel;
+    private readonly TransferService./* proto file generates this's name*/ client;
 
     public DataBaseAccess()
     {
-        rest.BaseAddress = new Uri("http://localhost:8080");
+        //rest.BaseAddress = new Uri("http://localhost:8080");
+        channel = GrpcChannel.ForAddress("http://localhost:8080"); 
+        client = new TransferService.TransferServiceClient(channel);
     }
 
     public async Task MakeTransfer(TransferRequestDTO request)
     {
         String json = JsonSerializer.Serialize(request);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
-        HttpResponseMessage message = await rest.PostAsync("transactions/",content);
+        //HttpResponseMessage message = await rest.PostAsync("transactions/",content);
         
         if (message.IsSuccessStatusCode)
         {
@@ -29,6 +37,10 @@ public class DataBaseAccess : IDataBaseAccess
         }
         else
         {
+            
+        }
+
+        {   
             // Handle errors here
             Console.WriteLine("Error: " + message.StatusCode);
         }
