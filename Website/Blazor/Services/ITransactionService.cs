f666ea9bb73a4efc9dd64a277580aa41d45a1e14
vlaad08 <62983956+vlaad08@users.noreply.github.com>
2023-11-24 13:38:39 +0100

    Test deposit before merge

--- Website/Blazor/Services/ITransactionService.cs
+++ Website/Blazor/Services/ITransactionService.cs
@@ -3,4 +3,6 @@ namespace Blazor.Services.Http;
 public interface ITransactionService
 {
     public Task transfer(String senderAccount_id, String recipientAccount_id, double amount, String message);
+
+    public Task deposit(string accountNumber, double amount);
 }
